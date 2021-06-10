14fa052ccf579fb00796775b5165d6487b04ae78
Samuel Ford <samuel.ford@icloud.com>
2021-06-10 09:53:19 -0400

    Initial Commit

---
+++ Feedback-SwiftUI-LimitedModalCapabilities/Feedback_SwiftUI_LimitedModalCapabilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_SwiftUI_LimitedModalCapabilitiesApp.swift
+//  Feedback-SwiftUI-LimitedModalCapabilities
+//
+//  Created by Samuel Ford on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_SwiftUI_LimitedModalCapabilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
